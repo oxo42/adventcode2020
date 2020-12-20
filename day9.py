6b8cf4c44fde1344ce34e2bf703bca6f6d3e6054
John Oxley <john.oxley@gmail.com>
2020-12-20 10:49:31 -0500

    Day 9 - Part 2

--- day9.py
+++ day9.py
@@ -16,11 +16,28 @@ def first_invalid(nums: List[int], spread: int) -> Tuple[int, List[int]]:
             return (nums[i], interesting)
 
 
+def find_weakness(target: int, nums: List[int]) -> int:
+    ln = len(nums)
+
+    # print(f"{ln}: {nums}")
+    for seg_size in range(3, ln + 1):
+        # print(f"=seg size {seg_size}")
+        for i in range(0, ln - seg_size + 1):
+            segment = nums[i : i + seg_size]
+            s = sum(segment)
+            # print(f"- {i}: {segment} = {s} -> {weakness}")
+            if s == target:
+                weakness = min(segment) + max(segment)
+                return weakness
+
+
 def main():
     nums = read_file()
-    invalid, interesting = first_invalid(nums, 5)
-    
+    invalid, interesting = first_invalid(nums, 25)
+
     print(invalid)
+
+    print(find_weakness(invalid, nums))
 
 
 if __name__ == "__main__":
