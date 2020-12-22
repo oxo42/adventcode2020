f31daa1ab7c97ba11cd103388e8e0ed2b27a6983
John Oxley <john.oxley@gmail.com>
2020-12-22 12:34:10 -0500

    added time and changed default log to debug

--- ac.py
+++ ac.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import click
+import time
 import logging
 from importlib import import_module
 
@@ -15,11 +16,14 @@ def input_file(day: str, dev: bool) -> str:
 @click.argument("day")
 @click.option("-d", "--dev", is_flag=True, default=False)
 def run(day: str, dev: bool) -> None:
-    log_level = logging.INFO if dev else logging.WARNING
+    log_level = logging.DEBUG if dev else logging.INFO
     logging.basicConfig(level=log_level)
     logging.info("Day %s, dev mode %s", day, dev)
     mod = import_module(f"day{day}")
+    start = time.perf_counter()
     mod.main(input_file(day, dev), dev)
+    end = time.perf_counter()
+    logging.info("Elapsed: %s", (start - end))
 
 
 if __name__ == "__main__":
--- day10.py
+++ day10.py
@@ -7,7 +7,7 @@ import functools
 
 
 def find_diffs(nums: List[int], src: int, j1: int, j3: int) -> Tuple[int, int]:
-    logging.info("src: %s", src)
+    logging.debug("src: %s", src)
     if src + 1 in nums:
         return find_diffs(nums, src + 1, j1 + 1, j3)
     if src + 2 in nums:
@@ -28,8 +28,8 @@ nums: List[int] = []
 def count_arrangements(
     src: int,
 ) -> int:
-    logging.info("src: %s", src)
-    logging.info("nums %s", nums)
+    logging.debug("src: %s", src)
+    logging.debug("nums %s", nums)
     if src == max(nums):
         return 1
     x = 0
@@ -45,7 +45,7 @@ def count_arrangements(
 
 def main(input_file: str, dev: bool) -> None:
     nums.extend(read_ints(input_file))
-    logging.info(nums)
+    logging.debug(nums)
     j1, j3 = find_diffs(nums, 0, 0, 0)
     print(f"1 jolt: {j1}")
     print(f"3 jolt: {j3}")
