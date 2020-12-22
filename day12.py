74b30e5409f86ac4037987b1c9a5fd5ae8583f81
John Oxley <john.oxley@gmail.com>
2020-12-22 16:31:05 -0500

    Day 12 - Part 2

--- day12.py
+++ day12.py
@@ -25,11 +25,16 @@ class Ship:
         self.y = 0
         self.direction = 90  # east
 
-    def forward(self, value: int) -> None:
-        d = self.direction % 360
+    def normalised_direction(self, value: int) -> str:
+        d = value % 360
         if d < 0:
             d = 360 - d
-        compass = {0: "N", 90: "E", 180: "S", 270: "W"}[d]
+        return d
+
+    def forward(self, value: int) -> None:
+        compass = {0: "N", 90: "E", 180: "S", 270: "W"}[
+            self.normalised_direction(self.direction)
+        ]
         self.move(Move(compass, value))
 
     def move(self, m: Move) -> None:
@@ -56,6 +61,54 @@ class Ship:
         return f"At ({self.x}, {self.y}) facing {self.direction}, manhattan distance {self.manhattan_distance}"
 
 
+class Ship2(Ship):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.wp_x = 10
+        self.wp_y = -1
+
+    def rotate(self, value: int) -> None:
+        d = self.normalised_direction(value)
+        if d == 180:
+            self.wp_x = -self.wp_x
+            self.wp_y = -self.wp_y
+        if d == 90:
+            y = self.wp_x
+            x = -self.wp_y
+            self.wp_x = x
+            self.wp_y = y
+        if d == 270:
+            y = -self.wp_x
+            x = self.wp_y
+            self.wp_x = x
+            self.wp_y = y
+
+    def forward(self, value: int) -> None:
+        for _ in range(value):
+            self.x += self.wp_x
+            self.y += self.wp_y
+
+    def move(self, m: Move) -> None:
+        if m.action == "N":
+            self.wp_y -= m.value
+        if m.action == "S":
+            self.wp_y += m.value
+        if m.action == "E":
+            self.wp_x += m.value
+        if m.action == "W":
+            self.wp_x -= m.value
+        if m.action == "F":
+            self.forward(m.value)
+        if m.action == "R":
+            self.rotate(m.value)
+        if m.action == "L":
+            self.rotate(-m.value)
+
+    def __str__(self):
+        return f"At ({self.x}, {self.y}) wp ({self.wp_x}, {self.wp_y}) manhattan distance {self.manhattan_distance}"
+
+
 def main(input_file: str, dev: bool) -> None:
     ship = Ship()
     moves = get_moves(input_file)
@@ -63,3 +116,11 @@ def main(input_file: str, dev: bool) -> None:
         ship.move(m)
 
     print(ship)
+
+    print("=" * 80)
+    ship2 = Ship2()
+    for m in moves:
+        ship2.move(m)
+        logging.debug("%s: %s", m, ship2)
+
+    print(ship2)
