648f081e30e39597104846677456c8dd43a64d39
John Oxley <john.oxley@gmail.com>
2020-12-22 12:31:30 -0500

    Day 10 - part 2

--- day10.py
+++ day10.py
@@ -3,6 +3,7 @@
 import logging
 from common import read_ints
 from typing import List, Tuple
+import functools
 
 
 def find_diffs(nums: List[int], src: int, j1: int, j3: int) -> Tuple[int, int]:
@@ -20,10 +21,34 @@ def find_diffs(nums: List[int], src: int, j1: int, j3: int) -> Tuple[int, int]:
     raise RuntimeError("Eh, whoops")
 
 
+nums: List[int] = []
+
+
+@functools.lru_cache(maxsize=None)
+def count_arrangements(
+    src: int,
+) -> int:
+    logging.info("src: %s", src)
+    logging.info("nums %s", nums)
+    if src == max(nums):
+        return 1
+    x = 0
+    if src + 1 in nums:
+        x += count_arrangements(src + 1)
+    if src + 2 in nums:
+        x += count_arrangements(src + 2)
+    if src + 3 in nums:
+        x += count_arrangements(src + 3)
+
+    return x
+
+
 def main(input_file: str, dev: bool) -> None:
-    nums = read_ints(input_file)
+    nums.extend(read_ints(input_file))
     logging.info(nums)
     j1, j3 = find_diffs(nums, 0, 0, 0)
     print(f"1 jolt: {j1}")
     print(f"3 jolt: {j3}")
     print(f"part 1: {j1*j3}")
+    arrangements = count_arrangements(0)
+    print(f"part 2: {arrangements}")
